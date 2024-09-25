df1801784a77dbc073179c9ee2ca6af721f18ecb
wnhhuong <23520590@gm.uit.edu.vn>
2024-09-25 23:32:36 +0700

    Bài tập tuần 3

---
+++ Bài tập tuần 3/File Explorer/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace File_Explorer
+{
+    partial class File_Explorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(File_Explorer));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tenFile = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.thongTin = new System.Windows.Forms.ListView();
+            this.column1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Back = new System.Windows.Forms.Button();
+            this.Forward = new System.Windows.Forms.Button();
+            this.HienDuongDi = new System.Windows.Forms.Label();
+            this.Path = new System.Windows.Forms.TextBox();
+            this.OpenBox = new System.Windows.Forms.Button();
+            this.Option = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.Option.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 47);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tenFile);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.thongTin);
+            this.splitContainer1.Panel2.Cursor = System.Windows.Forms.Cursors.No;
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(826, 460);
+            this.splitContainer1.SplitterDistance = 275;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tenFile
+            // 
+            this.tenFile.ImageIndex = 0;
+            this.tenFile.ImageList = this.imageList1;
+            this.tenFile.Location = new System.Drawing.Point(1, 2);
+            this.tenFile.Name = "tenFile";
+            this.tenFile.SelectedImageIndex = 0;
+            this.tenFile.Size = new System.Drawing.Size(273, 457);
+            this.tenFile.TabIndex = 2;
+            this.tenFile.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tenFile_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "photo-1588611164738-ce2bd6151ec4.png");
+            this.imageList1.Images.SetKeyName(1, "yuri-krupenin-XiE_PmKL-6U-unsplash.jpg");
+            // 
+            // thongTin
+            // 
+            this.thongTin.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.column1,
+            this.column2,
+            this.columnHeader1});
+            this.thongTin.HideSelection = false;
+            this.thongTin.Location = new System.Drawing.Point(1, 2);
+            this.thongTin.Name = "thongTin";
+            this.thongTin.Size = new System.Drawing.Size(546, 457);
+            this.thongTin.SmallImageList = this.imageList1;
+            this.thongTin.TabIndex = 3;
+            this.thongTin.UseCompatibleStateImageBehavior = false;
+            this.thongTin.View = System.Windows.Forms.View.Details;
+            // 
+            // column1
+            // 
+            this.column1.Text = "Name";
+            // 
+            // column2
+            // 
+            this.column2.Text = "Type";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Last Modified";
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(3, 12);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(44, 26);
+            this.Back.TabIndex = 2;
+            this.Back.Text = "<<";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // Forward
+            // 
+            this.Forward.Location = new System.Drawing.Point(48, 12);
+            this.Forward.Name = "Forward";
+            this.Forward.Size = new System.Drawing.Size(44, 26);
+            this.Forward.TabIndex = 3;
+            this.Forward.Text = ">>";
+            this.Forward.UseVisualStyleBackColor = true;
+            this.Forward.Click += new System.EventHandler(this.Forward_Click);
+            // 
+            // HienDuongDi
+            // 
+            this.HienDuongDi.AutoSize = true;
+            this.HienDuongDi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HienDuongDi.Location = new System.Drawing.Point(98, 17);
+            this.HienDuongDi.Name = "HienDuongDi";
+            this.HienDuongDi.Size = new System.Drawing.Size(42, 18);
+            this.HienDuongDi.TabIndex = 4;
+            this.HienDuongDi.Text = "Path:";
+            this.HienDuongDi.Click += new System.EventHandler(this.HienDuongDi_Click);
+            // 
+            // Path
+            // 
+            this.Path.Location = new System.Drawing.Point(146, 12);
+            this.Path.Name = "Path";
+            this.Path.Size = new System.Drawing.Size(561, 22);
+            this.Path.TabIndex = 5;
+            this.Path.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // OpenBox
+            // 
+            this.OpenBox.Location = new System.Drawing.Point(715, 9);
+            this.OpenBox.Name = "OpenBox";
+            this.OpenBox.Size = new System.Drawing.Size(92, 29);
+            this.OpenBox.TabIndex = 6;
+            this.OpenBox.Text = "Open";
+            this.OpenBox.UseVisualStyleBackColor = true;
+            this.OpenBox.Click += new System.EventHandler(this.OpenBox_Click);
+            // 
+            // Option
+            // 
+            this.Option.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Option.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.newToolStripMenuItem});
+            this.Option.Name = "Option";
+            this.Option.Size = new System.Drawing.Size(123, 124);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.cutToolStripMenuItem.Text = "Cut";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // File_Explorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 507);
+            this.Controls.Add(this.Path);
+            this.Controls.Add(this.Forward);
+            this.Controls.Add(this.OpenBox);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.HienDuongDi);
+            this.Controls.Add(this.Back);
+            this.Name = "File_Explorer";
+            this.Text = "File Explorer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.Option.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox Path;
+        private System.Windows.Forms.Label HienDuongDi;
+        private System.Windows.Forms.Button Forward;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.Button OpenBox;
+        private System.Windows.Forms.TreeView tenFile;
+        private System.Windows.Forms.ListView thongTin;
+        private System.Windows.Forms.ColumnHeader column1;
+        private System.Windows.Forms.ColumnHeader column2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ContextMenuStrip Option;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+    }
+}
+
---
+++ Bài tập tuần 3/File Explorer/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace File_Explorer
+{
+    public partial class File_Explorer : Form
+    {
+        Stack<string> backStack = new Stack<string>();
+        Stack<string> forwardStack = new Stack<string>();
+        string currentDirectory;
+
+        public static class FileClipboard
+        {
+            public static List<string> FilePaths { get; set; } = new List<string>();
+            public static bool IsCut { get; set; } = false;
+        }
+        public void ShowDrives()
+        {
+            tenFile.BeginUpdate();
+            string[] drives = Directory.GetLogicalDrives();
+            foreach (string adrive in drives)
+            {
+                TreeNode tn = new TreeNode(adrive);
+                tenFile.Nodes.Add(tn);
+            }
+            tenFile.EndUpdate();
+        }
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+            DirectoryInfo info = new DirectoryInfo(@"../..");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                tenFile.Nodes.Add(rootNode);
+            }
+        }
+        public File_Explorer()
+        {
+            InitializeComponent();
+            PopulateTreeView();
+            InitializeContextMenu();
+            this.tenFile.NodeMouseClick += new TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.OpenBox.Click += new EventHandler(this.OpenBox_Click);
+        }
+
+        private void InitializeContextMenu()
+        {
+            Option = new ContextMenuStrip();
+            Option.Items.Add("Sao chép", null, Copy_Click);
+            Option.Items.Add("Cắt", null, Cut_Click);
+            Option.Items.Add("Dán", null, Paste_Click);
+            Option.Items.Add("Xóa", null, Delete_Click);
+            Option.Items.Add("Tạo Thư Mục Mới", null, NewFolder_Click);
+            thongTin.ContextMenuStrip = Option;
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                try
+                {
+                    aNode = new TreeNode(subDir.Name, 0, 0);
+                    aNode.Tag = subDir;
+                    aNode.ImageKey = "folder";
+                    subSubDirs = subDir.GetDirectories();
+                    if (subSubDirs.Length != 0)
+                    {
+                        GetDirectories(subSubDirs, aNode);
+                    }
+
+                    nodeToAddTo.Nodes.Add(aNode);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine($"Access to the path {subDir.FullName} is denied: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error accessing directory {subDir.FullName}: {ex.Message}");
+                }
+            }
+        }
+        void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            thongTin.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "Directory"),
+              new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString()) };
+                item.SubItems.AddRange(subItems);
+                thongTin.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"),
+              new ListViewItem.ListViewSubItem(item, file.LastAccessTime.ToShortDateString()) };
+                item.SubItems.AddRange(subItems);
+                thongTin.Items.Add(item);
+            }
+
+            thongTin.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            Path.Text = nodeDirInfo.FullName;
+            OpenDirectory(nodeDirInfo.FullName);
+        }
+        private void LoadFilesFromDirectory(string path)
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                thongTin.Items.Clear();
+
+                foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                {
+                    ListViewItem item = new ListViewItem(dir.Name, 0);
+                    item.SubItems.Add("Directory");
+                    item.SubItems.Add(dir.LastAccessTime.ToShortDateString());
+                    thongTin.Items.Add(item);
+                }
+
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    ListViewItem item = new ListViewItem(file.Name, 1);
+                    item.SubItems.Add("File");
+                    item.SubItems.Add(file.LastAccessTime.ToShortDateString());
+                    thongTin.Items.Add(item);
+                }
+
+                thongTin.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void OpenDirectory(string path)
+        {
+            if (!string.IsNullOrEmpty(currentDirectory))
+            {
+                backStack.Push(currentDirectory);
+            }
+
+            LoadFilesFromDirectory(path);
+            currentDirectory = path;
+            forwardStack.Clear();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            if (backStack.Count > 0)
+            {
+                forwardStack.Push(currentDirectory);
+                string previousDirectory = backStack.Pop();
+                LoadFilesFromDirectory(previousDirectory); 
+                currentDirectory = previousDirectory; 
+                Path.Text = currentDirectory;
+            }
+        }
+
+        private void Forward_Click(object sender, EventArgs e)
+        {
+            if (forwardStack.Count > 0)
+            {
+                backStack.Push(currentDirectory);
+                string nextDirectory = forwardStack.Pop();
+                LoadFilesFromDirectory(nextDirectory);
+                currentDirectory = nextDirectory;
+                Path.Text = currentDirectory;
+            }
+        }
+
+        private void OpenBox_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                if (folderBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowser.SelectedPath;
+                    OpenDirectory(selectedPath);
+                    tenFile.Nodes.Clear();
+                    TreeNode rootNode = new TreeNode(selectedPath);
+                    rootNode.Tag = new DirectoryInfo(selectedPath);
+                    tenFile.Nodes.Add(rootNode);
+                    PopulateTreeViewFromPath(selectedPath);
+                }
+            }
+        }
+        private void PopulateTreeViewFromPath(string path)
+        {
+            DirectoryInfo info = new DirectoryInfo(path);
+            TreeNode rootNode = new TreeNode(info.Name);
+            rootNode.Tag = info;
+            GetDirectories(info.GetDirectories(), rootNode);
+            tenFile.Nodes.Add(rootNode);
+        }
+
+        private void CopyFiles()
+        {
+            FileClipboard.FilePaths.Clear();
+            foreach (ListViewItem item in thongTin.SelectedItems)
+            {
+                FileClipboard.FilePaths.Add(item.Tag.ToString());
+            }
+            FileClipboard.IsCut = false;
+        }
+
+        private void CutFiles()
+        {
+            FileClipboard.FilePaths.Clear();
+            foreach (ListViewItem item in thongTin.SelectedItems)
+            {
+                FileClipboard.FilePaths.Add(item.Tag.ToString());
+            }
+            FileClipboard.IsCut = true;
+        }
+
+        private void PasteFiles(string targetDirectory)
+        {
+            foreach (var filePath in FileClipboard.FilePaths)
+            {
+                string fileName = System.IO.Path.GetFileName(filePath);
+                string destPath = System.IO.Path.Combine(targetDirectory, fileName);
+
+                if (FileClipboard.IsCut)
+                {
+                    File.Move(filePath, destPath);
+                }
+                else
+                {
+                    File.Copy(filePath, destPath);
+                }
+            }
+            LoadFilesFromDirectory(targetDirectory);
+            LoadFilesFromDirectory(targetDirectory);
+        }
+
+        private void DeleteFiles()
+        {
+            foreach (ListViewItem item in thongTin.SelectedItems)
+            {
+                string filePath = item.Tag.ToString(); 
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+                else if (Directory.Exists(filePath))
+                {
+                    Directory.Delete(filePath, true);
+                }
+            }
+            LoadFilesFromDirectory(currentDirectory);
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            CopyFiles();
+        }
+
+        private void Cut_Click(object sender, EventArgs e)
+        {
+            CutFiles();
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            PasteFiles(currentDirectory);
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            DeleteFiles();
+        }
+
+        private void NewFolder_Click(object sender, EventArgs e)
+        {
+            CreateNewFolder(currentDirectory);
+        }
+
+        private void CreateNewFolder(string parentDirectory)
+        {
+            string currentDirectoryPath = Path.Text;
+
+            string newFolderName = "New Folder";
+            string newFolderPath = System.IO.Path.Combine(currentDirectoryPath, newFolderName); // Sử dụng Path.Combine
+
+            int count = 1;
+
+            while (Directory.Exists(newFolderPath))
+            {
+                newFolderPath = System.IO.Path.Combine(currentDirectoryPath, $"{newFolderName} ({count++})"); // Tạo tên thư mục mới nếu đã tồn tại
+            }
+
+            Directory.CreateDirectory(newFolderPath);
+            LoadFilesFromDirectory(currentDirectoryPath);
+        }
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e) { }
+        private void HienDuongDi_Click(object sender, EventArgs e) { }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e) { }
+        private void tenFile_AfterSelect(object sender, TreeViewEventArgs e) { }
+    }
+}
