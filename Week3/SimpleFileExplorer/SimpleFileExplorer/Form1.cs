8a27d411629f9f223be66ffc3790a12ca52e84a2
ngquocvuong23 <23521823@gm.uit.edu.vn>
2024-09-25 23:34:40 +0700

    FileExplorer

---
+++ Week3/SimpleFileExplorer/SimpleFileExplorer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleFileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path." })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(fbd.SelectedPath);
+                    txtPath.Text = fbd.SelectedPath;
+                }
+                    
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if(webBrowser.CanGoBack)    
+                webBrowser.GoBack();
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if(webBrowser.CanGoForward)
+                webBrowser.GoForward();
+        }
+    }
+}
